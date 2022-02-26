997e9522f03a447d92cc9bf2476cef6fdb54631c
Ying <yingxie@Ying.local>
2022-02-26 15:30:31 -0600

    turn off debug mode

--- mai_web/settings.py
+++ mai_web/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = ['sstradingapp.herokuapp.com']
 # 'https://sstradingapp.herokuapp.com'
