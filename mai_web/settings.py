8e3bc8460e91dbf1b9570e73df6801495904570d
Ying <yingxie@Ying.local>
2022-02-26 10:16:58 -0600

    Static file

--- mai_web/settings.py
+++ mai_web/settings.py
@@ -109,12 +109,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
