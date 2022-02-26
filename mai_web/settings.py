3c031f847b77420bebb5fe45b7eddfbd5b6a7ac0
Ying <yingxie@Ying.local>
2022-02-26 01:02:02 -0600

    static

--- mai_web/settings.py
+++ mai_web/settings.py
@@ -108,10 +108,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
