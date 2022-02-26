ef2ca7d14c04ff6430adc338ccdf4b757fb5fd9b
Ying <yingxie@Ying.local>
2022-02-25 23:30:27 -0600

    final commit for the site
    git add -A

--- mai_web/settings.py
+++ mai_web/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://sstradingapp.herokuapp.com/']
+ALLOWED_HOSTS = ['sstradingapp.herokuapp.com']
 # 'https://sstradingapp.herokuapp.com'
 
 
