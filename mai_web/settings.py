6c110fb6491fac3f4f610d5be0adde5270bf0fae
mablic <maihe88@gmail.com>
2022-05-12 22:41:25 -0500

    push to heroku

--- mai_web/settings.py
+++ mai_web/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sstradingapp.herokuapp.com']
 # 'sstradingapp.herokuapp.com'
 # 'https://sstradingapp.herokuapp.com'
 
